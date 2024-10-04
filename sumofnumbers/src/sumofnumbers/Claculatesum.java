f74308764aa6692114d05a46fa32530c8e4be615
ronal <ronal@MWEKAMBE>
2024-10-04 06:12:47 +0300

    first commit

---
+++ sumofnumbers/src/sumofnumbers/Claculatesum.java
@@ -0,0 +1,15 @@
+package sumofnumbers;
+
+public class Claculatesum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int sum = 0;
+	        for (int m = 1; m <= 10; m++) {
+	            sum =sum + m;
+	        }
+	        System.out.println("The sum of the first 10 natural numbers is: " + sum);
+	    }
+
+}
+
